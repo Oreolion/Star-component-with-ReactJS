db5300a50bd10c1ba0aa83a77473ba99a464c9e8
Oreolion <remyoreo11@gmail.com>
2023-09-08 05:07:39 -0700

    1 file modified

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,6 +1,33 @@
 // import { range } from './utils';
 
 function StarRating({ rating }) {
+
+
+  const range = (start, end, step = 1) => {
+    let output = [];
+    if (typeof end === "undefined") {
+        end = start;
+        start = 0;
+    }
+  
+    for (let i = start; i < end; i += step) {
+      output.push(i)
+    }
+  
+    return output;
+  };
+
+  console.log(range(rating));
+
+  
+  const printStar = range(rating).map(rating => {
+   return <img key={rating}
+    alt=""
+    className="gold-star"
+    src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
+  />
+
+  })
   /*
     Here's the markup for a single star:
     
@@ -15,7 +42,11 @@ function StarRating({ rating }) {
     If the rating is 4, we need 4 copies.
   */
 
-  return <div className="star-wrapper"></div>;
+  return (
+    <>
+     <div className="star-wrapper">{printStar}</div>
+    </>
+  )
 }
 
 export default StarRating;
